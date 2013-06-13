c49572499533d472cb1c50f65206c132d9e7520f
William Saults <wsaults@email.itt-tech.edu>
2013-06-12 23:32:28 -0500

    Bug fixes regarding JSONObject creation

--- Java1Project/src/com/fullsail/java1project/MainActivity.java
+++ Java1Project/src/com/fullsail/java1project/MainActivity.java
@@ -10,11 +10,20 @@
 package com.fullsail.java1project;
 
 import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
 import com.fullsail.lib.FetchJsonData;
 import com.fullsail.lib.Venue;
 
@@ -24,19 +33,49 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		// Create venues from the resouces
-		Log.i("Venue1 JSON string: ", getString(R.string.Venue1));
-		Log.i("Venue2 JSON string: ", getString(R.string.Venue2));
-		Log.i("Venue3 JSON string: ", getString(R.string.Venue2));
+		// Setup the linear layout
+		LinearLayout linearLayout = new LinearLayout(this);
+		linearLayout.setOrientation(LinearLayout.VERTICAL);
+		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+		linearLayout.setLayoutParams(layoutParams);
+		
+		// Add the button
+		Button button = new Button(this);
+		button.setText("Button");
+		linearLayout.addView(button);
+		
+		// Button event handler
+		button.setOnClickListener(new View.OnClickListener() { 
+
+			@Override
+			public void onClick(View v) {
+				try {
+					// Create venues from the resouces
+					Log.i("Venue1 JSON string: ", getString(R.string.Venue1));
+					Log.i("Venue2 JSON string: ", getString(R.string.Venue2));
+					Log.i("Venue3 JSON string: ", getString(R.string.Venue2));
+					
+					JSONObject jsonObject = new JSONObject(getString(R.string.Venue1));
+					Log.i("jsonObject: ", jsonObject.toString());
+					Venue venue = new Venue(jsonObject);
+				} catch (JSONException e1) {
+					// TODO Auto-generated catch block
+					Log.i("Error: ", e1.getMessage());
+					e1.printStackTrace();
+				}
+			}
+		});
 		
 		// Fetch the json data from the data file
 		try {
-			String json = FetchJsonData.jsonToStringFromAssetFolder("data", getBaseContext());
+			String json = FetchJsonData.jsonToStringFromAssetFolder("data.json", getBaseContext());
 			Log.i("JSON string: ", json);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		setContentView(linearLayout);
 	}
 
 	@Override
--- Java1Project/src/com/fullsail/lib/Venue.java
+++ Java1Project/src/com/fullsail/lib/Venue.java
@@ -9,6 +9,12 @@
  */
 package com.fullsail.lib;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
 // This class will be used to create location objects from the FourSquare API.
 /*
  * A venue is made up of...
@@ -26,5 +32,46 @@ package com.fullsail.lib;
  *  referralId: "v-1371082711"
  */
 public class Venue {
-
+	private String id;
+	private String name;
+	private JSONArray location;
+	private JSONArray categories;
+	
+	// Accessors and Mutators.
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public JSONArray getLocation() {
+		return location;
+	}
+	public void setLocation(JSONArray location) {
+		this.location = location;
+	}
+	public JSONArray getCategories() {
+		return categories;
+	}
+	public void setCategories(JSONArray categories) {
+		this.categories = categories;
+	}
+	
+	public Venue(JSONObject obj) throws JSONException {
+		this.id = obj.getString("id");
+		this.name = obj.getString("name");
+//		this.location = obj.getJSONArray("location");
+//		this.categories = obj.getJSONArray("categories");
+		
+		Log.i("id: ", id);
+		Log.i("name: ", name);
+//		Log.i("location: ", location.toString());
+//		Log.i("categories: ", categories.toString());
+	}
 }
