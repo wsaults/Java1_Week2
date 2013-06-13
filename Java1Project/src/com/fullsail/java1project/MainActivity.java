00aa367280a22e1b6c333ab63ff81d35f6c73428
William Saults <wsaults@email.itt-tech.edu>
2013-06-12 19:17:46 -0500

    Initial commit with package and class

---
+++ Java1Project/src/com/fullsail/java1project/MainActivity.java
@@ -0,0 +1,21 @@
+package com.fullsail.java1project;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ Java1Project/src/com/fullsail/lib/Location.java
@@ -0,0 +1,15 @@
+/*
+ * project 	Java1Project
+ * 
+ * package 	com.fullsail.lib
+ * 
+ * @author 	William Saults
+ * 
+ * date 	Jun 12, 2013
+ */
+package com.fullsail.lib;
+
+// This class will be used to create location objects from the FourSquare API.
+public class Location {
+
+}
