807e0a889071df3f5045e1f193456362fe259d9b
William Saults <wsaults@email.itt-tech.edu>
2013-06-13 20:56:24 -0500

    Added finishing touches. Added APK Added Cert

--- Java1Project/src/com/fullsail/java1project/MainActivity.java
+++ Java1Project/src/com/fullsail/java1project/MainActivity.java
@@ -10,39 +10,73 @@
 package com.fullsail.java1project;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.util.JsonReader;
+import android.content.Context;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.fullsail.lib.FetchJsonData;
 import com.fullsail.lib.Venue;
+import com.fullsail.lib.VenueEnum;
 
 public class MainActivity extends Activity {
-
+	
+	// Variables
+	Venue venues[] = new Venue[3];
+	LinearLayout linearLayout;
+	LinearLayout subLinearLayout;
+	LinearLayout.LayoutParams layoutParams;
+	TextView textView;
+	RadioGroup venueGroupOptions;
+	Context context = this;
+	Boolean areRadiosPoulated;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		areRadiosPoulated = false;
 		
 		// Setup the linear layout
-		LinearLayout linearLayout = new LinearLayout(this);
+		linearLayout = new LinearLayout(this);
 		linearLayout.setOrientation(LinearLayout.VERTICAL);
-		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+		layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
 		linearLayout.setLayoutParams(layoutParams);
 		
 		// Add the button
 		Button button = new Button(this);
-		button.setText("Button");
+		button.setText("Get JSON");
 		linearLayout.addView(button);
+		
+		// Add the text view
+		textView = new TextView(this);
+		textView.setText("The JSON data will appear here.");
+		linearLayout.addView(textView);
+		
+		// Setup the nested ll
+		subLinearLayout = new LinearLayout(this);
+		subLinearLayout.setOrientation(LinearLayout.VERTICAL);
+		subLinearLayout.setLayoutParams(layoutParams);
+		linearLayout.addView(subLinearLayout);
+		
+		// Add the text view
+		TextView optionsText = new TextView(this);
+		optionsText.setText("Choose a locaiton.");
+		subLinearLayout.addView(optionsText);
+		
+		// Radio group
+		venueGroupOptions = new RadioGroup(this);
+		subLinearLayout.addView(venueGroupOptions);
 		
 		// Button event handler
 		button.setOnClickListener(new View.OnClickListener() { 
@@ -50,14 +84,29 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				try {
-					// Create venues from the resouces
-					Log.i("Venue1 JSON string: ", getString(R.string.Venue1));
-					Log.i("Venue2 JSON string: ", getString(R.string.Venue2));
-					Log.i("Venue3 JSON string: ", getString(R.string.Venue2));
-					
+					// Create venues from the resouces					
 					JSONObject jsonObject = new JSONObject(getString(R.string.Venue1));
-					Log.i("jsonObject: ", jsonObject.toString());
-					Venue venue = new Venue(jsonObject);
+					venues[VenueEnum.VINDEX0.ordinal()] = new Venue(jsonObject);
+					
+					jsonObject = new JSONObject(getString(R.string.Venue2));
+					venues[VenueEnum.VINDEX1.ordinal()] = new Venue(jsonObject);
+					
+					jsonObject = new JSONObject(getString(R.string.Venue3));
+					venues[VenueEnum.VINDEX2.ordinal()] = new Venue(jsonObject);
+					
+					textView.setText("");
+					for (Venue venue : venues) {
+						textView.append("=== Venue ===\n");
+						textView.append("Venue id: " + venue.getId() + "\nVenue name: " + venue.getName() + "\n");
+						
+						if (!areRadiosPoulated) {
+							// Populate the radio buttons
+							RadioButton rb = new RadioButton(context);
+							rb.setText(venue.getName());
+							venueGroupOptions.addView(rb);
+						}
+					}	
+					areRadiosPoulated = true;
 				} catch (JSONException e1) {
 					// TODO Auto-generated catch block
 					Log.i("Error: ", e1.getMessage());
@@ -84,5 +133,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
---
+++ Java1Project/src/com/fullsail/lib/VenueEnum.java
@@ -0,0 +1,21 @@
+/*
+ * project 	Java1Project
+ * 
+ * package 	com.fullsail.lib
+ * 
+ * @author 	William Saults
+ * 
+ * date 	Jun 13, 2013
+ */
+package com.fullsail.lib;
+
+public enum VenueEnum {
+	VINDEX0(0),
+	VINDEX1(1),
+	VINDEX2(2);
+	
+	private final int value;
+	VenueEnum(int value) {
+		this.value = value;
+	}
+}
