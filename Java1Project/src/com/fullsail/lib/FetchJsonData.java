d6440c5e8d16b958c78c85a63907e5614a6d4b9c
William Saults <wsaults@email.itt-tech.edu>
2013-06-18 20:25:41 -0500

    Added getURLStringResponse

--- Java1Project/src/com/fullsail/lib/FetchJsonData.java
+++ Java1Project/src/com/fullsail/lib/FetchJsonData.java
@@ -9,11 +9,15 @@
  */
 package com.fullsail.lib;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.util.Log;
 
 /* Use this to parse.
  * String jsonLocation = converJsonToStringFromAssetFolder(your_filename, your_context);
@@ -31,4 +35,30 @@ public class FetchJsonData {
         file.close();
         return new String(data);
     }
+	
+	/*
+	 * Retrieve a response from the url destination
+	 */
+	public static String getURLStringResponse(URL url) {
+		String response = "";
+		try {
+			URLConnection connection = url.openConnection();
+			BufferedInputStream bin = new BufferedInputStream(connection.getInputStream());
+			
+			byte[] contentBytes = new byte[1024];
+			int bytesRead = 0;
+			StringBuffer responseBuffer = new StringBuffer();
+			
+			while ((bytesRead = bin.read(contentBytes)) != -1) {
+				response = new String(contentBytes, 0, bytesRead);
+				responseBuffer.append(response);
+			}
+			return responseBuffer.toString();
+			
+		} catch (Exception e) {
+			Log.e("URL response error.", "getURLStringResponse");
+		}
+		
+		return response;
+	}
 }
