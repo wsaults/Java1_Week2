ca0113daecb26c85c2783f20fb274bf5e049eab5
William Saults <wsaults@email.itt-tech.edu>
2013-07-30 20:36:48 -0500

    Added fragment class

--- Java1Project/gen/com/fullsail/java1project/R.java
+++ Java1Project/gen/com/fullsail/java1project/R.java
@@ -35,7 +35,7 @@ public final class R {
     }
     public static final class id {
         public static final int GridLayout1=0x7f090000;
-        public static final int action_settings=0x7f090014;
+        public static final int action_settings=0x7f090015;
         public static final int brandingImage=0x7f090009;
         public static final int celciusButton=0x7f090012;
         public static final int chooseCityLabel=0x7f09000d;
@@ -54,12 +54,14 @@ public final class R {
         public static final int tempMaxLabel=0x7f090004;
         public static final int tempMin=0x7f090005;
         public static final int tempMinLabel=0x7f090006;
+        public static final int weather_fragment=0x7f090014;
         public static final int webpageButton=0x7f090013;
     }
     public static final class layout {
         public static final int forecast_grid_layout=0x7f030000;
         public static final int mainlayout=0x7f030001;
         public static final int secondlayout=0x7f030002;
+        public static final int weatherfrag=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f080000;
---
+++ Java1Project/src/com/fullsail/java1project/WeatherFragment.java
@@ -0,0 +1,7 @@
+package com.fullsail.java1project;
+
+import android.app.Fragment;
+
+public class WeatherFragment extends Fragment {
+
+}
